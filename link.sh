98e3601a103b37f693678ddfec671dae97f5ee7f
Mukhammad Kharis <mkharis@users.noreply.github.com>
2026-08-02 07:50:28 +0700

    Change symbolic links to use absolute paths
    Update symbolic links to use absolute paths based on current directory.

--- link.sh
+++ link.sh
@@ -2,8 +2,8 @@
 
 for app in foot rofi sway swaylock waybar; do
     rm -rf ~/.config/$app
-    ln -s $app ~/.config/$app
+    ln -s "$(pwd)/$app" ~/.config/$app
 done
 
 rm -rf ~/Documents/scripts
-ln -s scripts ~/Documents/scripts
+ln -s "$(pwd)/scripts" ~/Documents/scripts
